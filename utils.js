484fdc9a24203638d99cf2d05aa1dad788a39b99
David Francisco <dfrancisco2677@gmail.com>
2022-05-16 16:54:49 -0700

    Building out my HTML bones for each page, login button redirects to login page.

---
+++ utils.js
@@ -0,0 +1,12 @@
+// import functions and grab DOM elements
+const loginButton = document.getElementById('login-page-button');
+const postButton = document.getElementById('post-button');
+const postDiv = document.getElementById('Posts');
+// let state
+
+
+// set event listeners 
+
+  // get user input
+  // use user input to update state 
+  // update DOM to reflect the new state
